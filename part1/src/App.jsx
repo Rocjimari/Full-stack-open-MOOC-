caa1e4ea3b6e81720155f32e509caf818ca03235
rocjimari <root@RocJimAri.localdomain>
2025-05-14 13:10:57 +0200

    Exercise 1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,49 +1,43 @@
 import { useState } from 'react'
 
-// estado de la app
-const Display = (props) => {
-    return (
-      <div>{props.counter}</div>
-    )
-}
 
-// componente button para los botones de la app
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
-  )
-}
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
+  
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral( neutral + 1)
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
 
-  const [ counter, setCounter ] = useState(0)
-
-
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <Display counter={counter}/>
+      <h1>Give Feedback</h1>
 
-      <Button 
-        onClick={increaseByOne}//controlador de eventos a traves de la propiedad onClick
-        text='plus'
-      />
-
-      <Button 
-        onClick={setToZero}
-        text='zero'
-      />
-
-      <Button 
-        onClick={decreaseByOne}
-        text='minus'
-      />
+      <Button handleClick={handleGoodClick} text='Good' />      
+      <Button handleClick={handleNeutralClick} text='Neutral' />      
+      <Button handleClick={handleBadClick} text='Bad' />
+      <h1>Statistics</h1>
+      good {good} <br />
+      neutral {neutral} <br />
+      bad {bad}
     </div>
   )
 }
+
 export default App
